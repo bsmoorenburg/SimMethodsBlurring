be41c5db78f26ea86ff6cbe88cf998202ea911fe
bsmoorenburg <82232987+bsmoorenburg@users.noreply.github.com>
2021-04-15 11:31:15 -0500

    Add files via upload
    Stripped down function file - may need to change the function name and such depending on whose laptop we use

---
+++ Image_Blurring.m
@@ -0,0 +1,51 @@
+function [] = Image_Blurring(option)
+
+format compact; 
+
+Image = imread(option);
+
+[rows, columns, channels] = size(Image);  
+
+subplot(3, 2, 1); 
+imshow(Image, []);
+axis on;
+title('Original Grayscale', 'FontSize', 12);
+n=1;
+blur=13;
+blur_count= 5;
+
+for a=1:1:blur_count
+    if channels>1
+       fprintf('ERROR: Image has %i channels, NOT GRAYSCALE IMAGE; will not render properly', channels)
+       break
+    end
+
+    filteredImage = zeros(size(Image));
+    Correction = floor(blur / 2);
+
+    for j = Correction + 1 : columns - Correction 
+        for i = Correction + 1 : rows - Correction
+            pixelSum = 0; 
+            for c = 1 : blur
+                ic = j + c - Correction - 1;
+                for r = 1 : blur
+                    ir = i + r - Correction - 1;
+                    pixelSum = pixelSum + double(Image(ir, ic))/blur^2;
+                end
+            end
+            filteredImage(i, j) = pixelSum;
+        end
+    end
+
+    n=n+1;
+
+    subplot(3, 2, n);
+    imshow(filteredImage, []);
+    caption = sprintf('Image width filter of %d', blur);
+    title(caption, 'FontSize', 12);
+    axis on;
+
+    blur=blur+10;
+
+end
+end
