0d64518438ee2f0f97b4f113d70f85f13742f998
HailtheWhale <82235221+HailtheWhale@users.noreply.github.com>
2021-04-15 11:24:56 -0500

    Add files via upload
    Script to Call our Function

---
+++ Project_Image_Caller.m
@@ -0,0 +1,17 @@
+% Sim Methods Project: Grayscale Image Blurring 
+% Team 6: Benjamins Avants, Brennon Broussard, Bailey Smoorenburg, Cameron
+% Cage, Donovan Gegg 
+% 4/22/2021 
+% Script to Give INPUT OPTIONS
+
+clc;
+clear;
+
+Option_1= 'view.png'
+Option_2= 'image2'
+Option_3= 'image3'
+Option_4= 'image4' 
+
+
+fprintf('Call the following Function to Blur the previous images \n Blur_Image(INPUT_IMAGE)')
+
