06f4d329af66a10538bcc105f081b7d92084b17a
HailtheWhale <82235221+HailtheWhale@users.noreply.github.com>
2021-04-20 11:12:16 -0500

    Update Project_Image_Caller.m

--- Project_Image_Caller.m
+++ Project_Image_Caller.m
@@ -8,14 +8,34 @@ clc;
 clear;
 
 Option_1= 'GSlsu.png'
-Option_2= 'GSmike.png'
-Option_3= 'GStigers.png' 
+Option_1_read = imread(Option_1);
+[rows, columns, channels] = size(Option_1_read); 
+subplot(1, 3, 1); 
+imshow(Option_1_read, []);
+title('Option 1', 'FontSize', 12);
 
+%%%%%%%%%%%%%%%%%%%%%%%
 
+Option_2= 'GSmike2.png'
+Option_2_read = imread(Option_2);
+[rows, columns, channels] = size(Option_2_read); 
+subplot(1, 3, 2); 
+imshow(Option_2_read, []);
+title('Option 2', 'FontSize', 12);
 
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+Option_3= 'GStigers.png'
+Option_3_read = imread(Option_3);
+[rows, columns, channels] = size(Option_3_read); 
+subplot(1, 3, 3); 
+imshow(Option_3_read, []);
+title('Option 3', 'FontSize', 12);
+
+%%%%%%%%%%%%%%%%%%%%%%%%
 
 fprintf('Which of the previous options would you like to Blur?  \n')
 
-x=input('Which image will you select?')
+option=input('Which image will you select?')
 
-Image_Blurring(x);
+Image_Blurring(option);
